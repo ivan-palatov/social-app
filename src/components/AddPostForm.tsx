922cff3f03e5f5d393c2d8da026447d66a5acf74
Иван Палатов <ipalatov@pm.me>
2022-01-27 12:24:01 +0700

    Add Modal component, improve AddPostForm component

--- src/components/AddPostForm.tsx
+++ src/components/AddPostForm.tsx
@@ -1,8 +1,11 @@
+import { mdiLink } from "@mdi/js";
+import Icon from "@mdi/react";
 import React, { useState } from "react";
 import { Button } from "react-bulma-components";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, createPost } from "../firebase";
 import { useAppSelector } from "../hooks";
+import Modal from "./Modal";
 
 interface IProps {}
 
@@ -10,6 +13,7 @@ const AddPostForm: React.FC<IProps> = () => {
   const state = useAppSelector((state) => state.user);
   const [body, setBody] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [user] = useAuthState(auth);
 
@@ -26,27 +30,57 @@ const AddPostForm: React.FC<IProps> = () => {
   }
 
   return (
-    <form className="box" noValidate onSubmit={savePost}>
-      <div className="field">
-        <div className="control">
-          <textarea
-            id="body"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            className="textarea"
-            placeholder="Поделитесь своими мыслями..."
-            rows={2}
-          />
-          <Button
-            className="is-success"
-            style={{ margin: "10px 0" }}
-            disabled={loading}
-          >
-            Опубликовать
-          </Button>
-        </div>
-      </div>
-    </form>
+    <>
+      <article className="media">
+        <figure className="media-left">
+          <p className="image is-64x64">
+            <img src={state.user?.avatar} alt="Аватар" className="is-rounded" />
+          </p>
+        </figure>
+        <form className="media-content" noValidate onSubmit={savePost}>
+          <div className="field">
+            <div className="control">
+              <textarea
+                id="body"
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                className="textarea"
+                placeholder="Поделитесь своими мыслями..."
+                rows={2}
+              />
+            </div>
+          </div>
+          <nav className="level">
+            <div className="level-left">
+              <div className="level-item">
+                <Button
+                  className="is-success"
+                  style={{ margin: "10px 0" }}
+                  disabled={loading}
+                >
+                  Опубликовать
+                </Button>
+              </div>
+              <div className="level-item">
+                <span
+                  className="icon is-clickable"
+                  onClick={() => setIsModalOpen(true)}
+                >
+                  <Icon path={mdiLink} />
+                </span>
+              </div>
+            </div>
+          </nav>
+        </form>
+      </article>
+
+      <Modal
+        isActive={isModalOpen}
+        onClickOutside={() => setIsModalOpen(false)}
+      >
+        <div>Перетащите сюда файл...</div>
+      </Modal>
+    </>
   );
 };
 
---
+++ src/components/Modal.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface IProps {
+  isActive?: boolean;
+  onClickOutside: () => void;
+}
+
+const Modal: React.FC<IProps> = ({
+  children,
+  isActive = false,
+  onClickOutside,
+}) => {
+  return ReactDOM.createPortal(
+    <div className={isActive ? "modal is-active" : "modal"}>
+      <div className="modal-background" onClick={onClickOutside}></div>
+      <div className="modal-content">
+        <div className="box">{children}</div>
+      </div>
+      <button
+        className="modal-close is-large"
+        aria-label="close"
+        onClick={onClickOutside}
+      ></button>
+    </div>,
+    document.body
+  );
+};
+
+export default Modal;
